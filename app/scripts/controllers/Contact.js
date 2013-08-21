cdc83fd3ca44e4521728a1bce3519d65d25c2382
David Woods <david@birnamdesigns.com>
2013-08-20 23:27:11 -0400

    renamed Contact.js again

--- app/scripts/controllers/Contact.js
+++ app/scripts/controllers/Contact.js
@@ -3,6 +3,8 @@
 angular.module('davidkwoodsApp')
         .controller('ContactCtrl', function ($scope, $resource, PageContent) {
             PageContent.getPage("contact").then(function(ret) {
-                angular.extend($scope, ret);
+                if (ret && ret.data && ret.data.rows && ret.data.rows.length > 0) {
+                    angular.extend($scope, ret.data.rows[0].value);
+                }
             });
         });
